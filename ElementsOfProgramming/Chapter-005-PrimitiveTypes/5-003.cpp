21c0de01f283b0815ac97c657842b621d82598bd
Craig Bennett <prefrontal@gmail.com>
2015-10-10 00:16:23 -0700

    Complete 5-003

--- ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-003.cpp
+++ ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-003.cpp
@@ -4,22 +4,44 @@
 // Write a function that takes a 64-bit word x and returns 
 // a 64-bit word consisting of the bits of x in reverse order.
 
+#include <cmath>
 #include <iostream>
 
-int64_t ReverseBits (const int64_t input)
+void DisplayBits (const uint64_t input)
 {
-	int64_t output = 0;
+	for (int i = 0; i < 64; i++)
+		std::cout << ((input >> (63-i)) & 1);	
 	
+	std::cout << std::endl;
+}
 
+uint64_t ReverseBits (const uint64_t input)
+{
+	uint64_t output = 0;
+	
+	for (int i = 0; i < 64; i++)
+		output |= ( ((input >> i) & 1) << (63-i));
 	
 	return output;	
 }
 
 int main (int argc, char *argv[]) 
 {
-	// Function Tests
-	bool passed = true;
+	// Test the ReverseBits function
+	bool passedTests = true;
 	
+	// Test every possible bit individually
+	for (uint64_t i = 0; i < 64; i++)
+	{
+		uint64_t initialValue = std::pow (2, i);
+		uint64_t trueValue = std::pow (2, (63-i));
+		uint64_t testValue = ReverseBits (initialValue);
+
+		std::cout << initialValue << " - " << trueValue << " - " << testValue << std::endl;
+		
+		if (testValue != trueValue)
+			passedTests = false;
+	}
 	
-	
+	std::cout << "Test status: " << passedTests << std::endl;
 }
