2e4b487bb8d4941499beedb1ea8166741ad58500
Craig Bennett <prefrontal@gmail.com>
2015-10-14 00:03:21 -0700

    Answer 5-002 and 5-010

--- ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-002.cpp
+++ ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-002.cpp
@@ -4,19 +4,46 @@
 // Implement code that takes as input a 64-bit integer and swaps
 // the bits in that integer at indices i and j
 
+#include <cmath>
 #include <iostream>
 
-int64_t SwapBits (const int64_t input, const int i, const int j)
+uint64_t SwapBits (const uint64_t input, const int i, const int j)
 {
-	int64_t output = input;
+	uint64_t output = input;
 	
-	if (((input >> i) & 1) != ((input >> j) & 1)
-		// Swap bits in output
+	if (((input >> i) & 1L) != ((input >> j) & 1L))
+	{
+		output ^= (1L << i);
+		output ^= (1L << j);	
+	}
 	
 	return output;	
 }
 
+void DisplayBits (const uint64_t input)
+{
+	for (int i = 0; i < 64; i++)
+		std::cout << ((input >> (63-i)) & 1);	
+	
+	std::cout << std::endl;
+}
+
 int main (int argc, char *argv[]) 
 {
+	bool passedTests = true;
+		
+	// Test every possible bit state individually
+	for (int i = 0; i < 64; i++)
+	{
+		uint64_t initialValue = 1;
+		uint64_t trueValue = std::pow (2, i);
+		uint64_t testValue = SwapBits (initialValue, 0, i);
+
+		std::cout << initialValue << " - " << trueValue << " - " << testValue << std::endl;
+		
+		if (testValue != trueValue)
+			passedTests = false;
+	}
 	
+	std::cout << "Test status: " << passedTests << std::endl;
 }
--- ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-010.cpp
+++ ElementsOfProgramming/Chapter-005-PrimitiveTypes/5-010.cpp
@@ -4,9 +4,92 @@
 // Write a function which takes an integer K and returns the integer 
 // corresponding to the digits of K written in reverse order.
 
+#include <cmath>
 #include <iostream>
+#include <vector>
+
+// We could do many different strategies here
+// One would be to convert to a string and reverse the string
+// I am going to do something a bit different and reverse a vector
+	
+// Reverse a 32-bit integer
+// Output may be greater than a 32-bit integer can hold
+// To prevent problems use a 64-bit value to hold the output
+int64_t ReverseInteger (const int32_t input)
+{
+	int32_t temp = input;
+	int64_t output = 0;
+	std::vector<int> digits;
+	
+	bool negativeFlag = false;
+	
+	// If a number is negative then make it positive
+	// We will flip it back the right way at the end
+	if (temp < 0)
+	{
+		negativeFlag = true;
+		temp = std::abs (temp);	
+	}
+	
+	// Capture the integer digits and reverse them
+	while (temp > 0)
+	{
+		digits.push_back (temp % 10);
+		temp /= 10;
+	}
+
+	std::reverse (digits.begin(), digits.end());
+	
+	// Build up the output integer
+	for (int i = 0; i < digits.size(); i++)
+	{
+		output += digits[i] * std::pow(10, i);
+	}
+	
+	// If we flipped a negative value earlier then make things right
+	if (negativeFlag)
+		output *= -1;
+	
+	return output;
+}
 
 int main (int argc, char *argv[]) 
 {
+	// Run some basic tests to check functionality
+	bool passedTests = true;
 	
+	if (0 != ReverseInteger(0))
+		passedTests = false;
+		
+	if (1 != ReverseInteger(1))
+		passedTests = false;
+		
+	if (-1 != ReverseInteger(-1))
+		passedTests = false;
+		
+	if (1 != ReverseInteger(10))
+		passedTests = false;
+	
+	if (-1 != ReverseInteger(-10))
+		passedTests = false;
+		
+	if (1 != ReverseInteger(100))
+		passedTests = false;
+	
+	if (-1 != ReverseInteger(-100))
+		passedTests = false;
+		
+	if (8005 != ReverseInteger(5008))
+		passedTests = false;
+		
+	if (-8005 != ReverseInteger(-5008))
+		passedTests = false;
+		
+	if (7463847412 != ReverseInteger(2147483647))
+		passedTests = false;
+		
+	if (-7463847412 != ReverseInteger(-2147483647))
+		passedTests = false;
+
+	std::cout << "Test status: " << passedTests << std::endl;
 }
