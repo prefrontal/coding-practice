b74ff883f8e5c8ba1e351e48085fd030abaf6f77
Craig Bennett <prefrontal@gmail.com>
2015-11-04 20:36:58 -0800

    RaiseMan ArrayController

---
+++ Cocoa Programming For OSX/009-RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Craig Bennett on 11/4/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+import Foundation
+
+class Employee : NSObject
+{
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
