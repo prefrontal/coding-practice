18d47be86f60c4126247870e84cb7cc2a68a6ef0
Craig Bennett <prefrontal@gmail.com>
2015-11-07 00:33:54 -0800

    AFNetworking Example

---
+++ Miscellaneous OSX/AFNetworkingExample/AFNetworkingExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFNetworkingExample
+//
+//  Created by Craig Bennett on 11/6/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
