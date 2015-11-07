8528dd1ba9e3d7e13e481776bc1ff08de874a5c6
Craig Bennett <prefrontal@gmail.com>
2015-11-06 20:01:40 -0800

    Add Chapter 11 and reorganize

---
+++ Cocoa Programming For OSX/011-RaiseMan-Undo/RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Craig Bennett on 11/1/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
