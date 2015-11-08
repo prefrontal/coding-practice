19499c1b7165eb2e250187e1ea88aec2f4dab286
Craig Bennett <prefrontal@gmail.com>
2015-11-07 19:55:06 -0800

    Finish Chapter 11

--- Cocoa Programming For OSX/011-RaiseMan-Undo/RaiseMan/Document.swift
+++ Cocoa Programming For OSX/011-RaiseMan-Undo/RaiseMan/Document.swift
@@ -8,10 +8,77 @@
 
 import Cocoa
 
-class Document: NSDocument
+private var KVOContext: Int = 0
+
+class Document: NSDocument, NSWindowDelegate
 {
 
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var arrayController: NSArrayController!
+
     var employees: [Employee] = []
+    {
+        willSet
+        {
+            for employee in employees
+            {
+                stopObservingEmployee (employee)
+            }
+        }
+        didSet
+        {
+            for employee in employees
+            {
+                startObservingEmployee (employee)
+            }
+        }
+    }
+
+    // MARK: - Actions
+
+    @IBAction func addEmployee (sender: NSButton)
+    {
+        let windowController = windowControllers[0]
+        let window = windowController.window!
+
+        let endedEditing = window.makeFirstResponder(window)
+
+        if !endedEditing
+        {
+            print ("Unable to end editing")
+            return
+        }
+
+        let undo: NSUndoManager = undoManager!
+
+        // Has an edit already occurred in this event?
+        if undo.groupingLevel > 0
+        {
+            // Close the last group
+            undo.endUndoGrouping()
+            // Open a new group
+            undo.beginUndoGrouping()
+        }
+
+        // Create the object
+        let employee = arrayController.newObject() as! Employee
+
+        // Add it to the array controller's contentArray
+        arrayController.addObject(employee)
+
+        // Re-sort (in case the user has sorted a column)
+        arrayController.rearrangeObjects()
+
+        // Get the sorted array
+        let sortedEmployees = arrayController.arrangedObjects as! [Employee]
+
+        // Find the object just added
+        let row = sortedEmployees.indexOf (employee)!
+
+        // Begin the edit in the first column
+        print ("Starting edit of \(employee) in row \(row)")
+        tableView.editColumn (0, row: row, withEvent: nil, select: true)
+    }
 
     // MARK: - Controller Methods
 
@@ -90,5 +157,50 @@ class Document: NSDocument
         employees.removeAtIndex(index)
     }
 
+    // Mark: - Key Value Observing
+
+    func startObservingEmployee (employee: Employee)
+    {
+        employee.addObserver (self, forKeyPath: "name", options: .Old, context: &KVOContext)
+        employee.addObserver (self, forKeyPath: "raise", options: .Old, context: &KVOContext)
+    }
+
+    func stopObservingEmployee (employee: Employee)
+    {
+        employee.removeObserver(self, forKeyPath: "name", context: &KVOContext)
+        employee.removeObserver(self, forKeyPath: "raise", context: &KVOContext)
+    }
+
+    override func observeValueForKeyPath (keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>)
+    {
+        if context != &KVOContext
+        {
+            // If the context does not match then this method is intended for the superclass
+            super.observeValueForKeyPath (keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+
+        if let keyPath = keyPath, object = object, change = change
+        {
+            var oldValue: AnyObject? = change[NSKeyValueChangeOldKey]
+
+            if oldValue is NSNull
+            {
+                oldValue = nil
+            }
+
+            let undo: NSUndoManager = undoManager!
+            undo.prepareWithInvocationTarget(object).setValue(oldValue, forKeyPath: keyPath)
+
+            print ("oldValue=\(oldValue)")
+        }
+    }
+
+    // MARK: - NSWindowDelegate
+
+    func windowWillClose(notification: NSNotification)
+    {
+        employees = []
+    }
 }
 
