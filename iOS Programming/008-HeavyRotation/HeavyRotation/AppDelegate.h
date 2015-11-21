aa7823d75a3de9d9d335439751418f815f3cfcb2
Craig Bennett <prefrontal@gmail.com>
2015-11-21 14:18:46 -0800

    Finish Chapter Eight

---
+++ iOS Programming/008-HeavyRotation/HeavyRotation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeavyRotation
+//
+//  Created by Craig Bennett on 11/21/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Programming/008-HeavyRotation/HeavyRotation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HeavyRotation
+//
+//  Created by Craig Bennett on 11/21/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
