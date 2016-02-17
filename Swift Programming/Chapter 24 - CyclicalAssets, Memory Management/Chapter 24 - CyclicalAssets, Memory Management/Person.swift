79290e2a1c8f78d4761f901a058b2ad23e8cb77a
Craig Bennett <prefrontal@gmail.com>
2016-02-16 22:47:32 -0800

    Swift Chapter 24

--- Swift Programming/Chapter 24 - CyclicalAssets, Memory Management/Chapter 24 - CyclicalAssets, Memory Management/Person.swift
+++ Swift Programming/Chapter 24 - CyclicalAssets, Memory Management/Chapter 24 - CyclicalAssets, Memory Management/Person.swift
@@ -11,19 +11,40 @@ import Foundation
 class Person: CustomStringConvertible
 {
     let name:String
+    let accountant = Accountant()
+    var assets = [Asset]()
 
     var description:String
-        {
-            return "Person(\(name))"
+    {
+        return "Person(\(name))"
     }
 
     init (name:String)
     {
         self.name = name
+        
+        accountant.netWorthChangedHandler =
+        {
+            [weak self] netWorth in
+            self?.netWorthDidChange (netWorth)
+            return
+        }
     }
 
     deinit
     {
         print ("\(self) is being deallocated")
+    }
+    
+    func takeOwnershipOfAsset (asset:Asset)
+    {
+        asset.owner = self
+        assets.append (asset)
+        accountant.gainedNewAsset (asset)
+    }
+    
+    func netWorthDidChange (netWorth:Double)
+    {
+        print ("The net worth of \(self) is now \(netWorth)")
     }
 }
