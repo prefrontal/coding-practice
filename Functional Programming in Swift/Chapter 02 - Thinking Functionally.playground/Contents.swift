0866af4bba3dfb35bd599f49a68e604e2ce0ad44
Craig Bennett <prefrontal@gmail.com>
2016-02-19 23:47:04 -0800

    Continue Functional Swift Chapter 2

--- Functional Programming in Swift/Chapter 02 - Thinking Functionally.playground/Contents.swift
+++ Functional Programming in Swift/Chapter 02 - Thinking Functionally.playground/Contents.swift
@@ -21,3 +21,39 @@ func inRange2 (target:Position, ownPosition:Position, range:Distance) -> Bool
     let targetDistance = sqrt(dx*dx + dy*dy)
     return targetDistance <= range
 }
+
+// Add a minimum distance constraint
+let minimumDistance:Distance = 2.0
+
+func inRange3 (target:Position, ownPosition:Position, range:Distance) -> Bool
+{
+    let dX = ownPosition.x - target.x
+    let dY = ownPosition.y - target.y
+    let targetDistance = sqrt(dX*dX + dY*dY)
+    return targetDistance <= range && targetDistance > minimumDistance
+}
+
+// Add a minimum distance from friendly ships as well
+
+func inRange4 (target:Position, ownPosition:Position, friendly:Position, range:Distance) -> Bool
+{
+    let dX = ownPosition.x - target.x
+    let dY = ownPosition.y - target.y
+    let targetDistance = sqrt(dX*dX + dY*dY)
+
+    let friendlyDx = friendly.x - target.x
+    let friendlyDy = friendly.y - target.y
+    let friendlyDistance = sqrt(friendlyDx*friendlyDx + friendlyDy*friendlyDy)
+
+    return targetDistance <= range && targetDistance > minimumDistance && friendlyDistance > minimumDistance
+}
+
+// As this code grows in complexity it becomes harder and harder to maintain. Time to break it up...
+// The key philosophy underlying functional programming is that functions are values
+
+typealias Region = Position -> Bool
+
+func Circle (radius:Distance) -> Region
+{
+    return {point in sqrt(point.x * point.x + point.y*point.y) <= radius}
+}
