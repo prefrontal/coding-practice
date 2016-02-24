8270e1351fb82a8e9ee55588b1832b286e955bbe
Craig Bennett <prefrontal@gmail.com>
2016-02-23 23:48:37 -0800

    More Functional Swift Chapter 4

--- Functional Programming in Swift/Chapter 04 - Map, Filter, Reduce.playground/Contents.swift
+++ Functional Programming in Swift/Chapter 04 - Map, Filter, Reduce.playground/Contents.swift
@@ -29,3 +29,33 @@ func doubleArray1 (xs:[Int]) -> [Int]
 
     return result
 }
+
+// Make a more general, abstract pattern
+
+func computeIntArray (xs:[Int], f: Int -> Int) -> [Int]
+{
+    var result:[Int] = []
+
+    for x in xs
+    {
+        result.append (f(x))
+    }
+
+    return result
+}
+
+// Now use the general solution
+
+func doubleArray2 (xs:[Int]) -> [Int]
+{
+    return computeIntArray (xs) {x in x * 2}
+}
+
+// You can even use it to calculate booleans
+// But, there is a type error here
+
+func isEvenArray (xs:[Int]) -> [Bool]
+{
+    computeIntArray (xs) {x in x % 2 == 0}
+}
+
