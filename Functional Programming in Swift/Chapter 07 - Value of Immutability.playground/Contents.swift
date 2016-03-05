500a2f578eecabe4e364aa2012aa20a677de2f59
Craig Bennett <prefrontal@gmail.com>
2016-03-05 14:19:17 -0800

    Start Functional Swift Chapter 7

---
+++ Functional Programming in Swift/Chapter 07 - Value of Immutability.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Functional Programming in Swift - Chapter 7
+
+/* The Value of Immutability */
+
+import Foundation
+
+// -------------------------------------------------------------------------------------------------------------------
+
+// My...remark is that our intellectual powers are rather geared to master static relations and that
+// our powers to visualize processes evolving in time are poorly developed." - Dikstra
+
+var x:Int = 1 // mutable
+var y:Int = 2 // immutable
+
+// Mutability can be considered harmful
