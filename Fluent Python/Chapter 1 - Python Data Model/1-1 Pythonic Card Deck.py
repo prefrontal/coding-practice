2b4eece6dc5ee58529df29359a0893684133b06f
Craig Bennett <prefrontal@gmail.com>
2016-05-29 14:14:56 -0700

    Chapter 1 of Fluent Python

---
+++ Fluent Python/Chapter 1 - Python Data Model/1-1 Pythonic Card Deck.py
@@ -0,0 +1,35 @@
+# Pythonic Card Deck
+
+# By using standard Python special methods a class can inherit 
+# a broad range of standard functionality automatically
+
+import collections
+
+Card = collections.namedtuple('Card',['rank','suit'])
+
+class FrenchDeck:
+	ranks = [str(n) for n in range(2,11)] + list('JQKA')
+	suits = 'spades diamonds clubs hearts'.split()
+	
+	def __init__(self):
+		self._cards = [Card(rank,suit) for suit in self.suits
+									   for rank in self.ranks]
+									
+	def __len__(self):
+		return len(self._cards)
+		
+	def __getitem__(self,position):
+		return self._cards[position]
+		
+deck = FrenchDeck()
+
+print (len(deck))
+
+print (deck[0])
+
+# Supports slicing
+print (deck[:3])
+
+# Supports StopIteration
+for card in deck:
+	print (card)
