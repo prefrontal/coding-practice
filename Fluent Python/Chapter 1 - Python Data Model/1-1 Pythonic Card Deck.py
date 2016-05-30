3a30f880c49ec5a92d364f93239debd2e0763a0e
Craig Bennett <prefrontal@gmail.com>
2016-05-29 17:21:00 -0700

    Update Fluent Python answers for PEP8

--- Fluent Python/Chapter 1 - Python Data Model/1-1 Pythonic Card Deck.py
+++ Fluent Python/Chapter 1 - Python Data Model/1-1 Pythonic Card Deck.py
@@ -5,31 +5,32 @@
 
 import collections
 
-Card = collections.namedtuple('Card',['rank','suit'])
+Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
-	ranks = [str(n) for n in range(2,11)] + list('JQKA')
-	suits = 'spades diamonds clubs hearts'.split()
-	
-	def __init__(self):
-		self._cards = [Card(rank,suit) for suit in self.suits
-									   for rank in self.ranks]
-									
-	def __len__(self):
-		return len(self._cards)
-		
-	def __getitem__(self,position):
-		return self._cards[position]
-		
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+# Test the FrenchDeck class implementation
 deck = FrenchDeck()
 
+# Supports length and index selection
 print (len(deck))
-
 print (deck[0])
 
 # Supports slicing
 print (deck[:3])
 
-# Supports StopIteration
+# Supports Iteration
 for card in deck:
-	print (card)
+    print (card)
--- Fluent Python/Chapter 1 - Python Data Model/1-2 Vector Manipulation.py
+++ Fluent Python/Chapter 1 - Python Data Model/1-2 Vector Manipulation.py
@@ -1,34 +1,37 @@
 from math import hypot
 
+
 class Vector:
-	
-	def __init__(self, x=0, y=0):
-		self.x = x
-		self.y = y
-		
-	def __repr__(self):
-		# __repr__ get the string representation of the object
-		return `Vector:(%r, %r,) % (self.x, self.y)
-		
-	def __abs__(self):
-		return hypot(self.x, self.y)
-		
-	def __bool__(self):
-		# Used to determine if objecct is Truthy or Falsey
-		return bool(abs(self))
-		
-	def __add__(self, other):
-		# Used when the + operator is present
-		x = self.x + other.x
-		y = self.y + other.y
-		return Vector(x, y)
-		
-	def __mul__(self, scalar):
-		# Used when the * operator is present
-		return Vector(self.x * scalar, self.y * scalar)
-		
-vector1 = Vector(4,5)
-vector2 = Vector (5,6)
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        # Obtain the string representation of the object
+        return "Vector(%r, %r)" % (self.x, self.y)
+
+    def __abs__(self):
+        # Return the absolute value of an object
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        # Used to determine if object is Truthy or Falsey
+        return bool(abs(self))
+
+    def __add__(self, other):
+        # Used when the + operator is present
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        # Used when the * operator is present
+        return Vector(self.x * scalar, self.y * scalar)
+
+
+# Test Vector class implementation
+vector1 = Vector(4, 5)
+vector2 = Vector(5, 6)
 
 print (vector1)
 print (vector2)
